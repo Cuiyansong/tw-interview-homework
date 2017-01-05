c676e30a4da162e35cbb6c3578690050f6b34226
cuiyansong <yscui@thoughtworks.com>
2017-01-05 20:03:45 +0800

    add file loader ,but not finish

--- vscode/conference-track-management/Program.cs
+++ vscode/conference-track-management/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var content = FileLoader.Load();
+            System.Console.WriteLine(content);
         }
     }
 }
